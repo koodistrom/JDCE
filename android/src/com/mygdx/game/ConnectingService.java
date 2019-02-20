197b214a03b907fe3fa88e75cd83bd9e48986a8b
Jaakko <j.i.mantyla@gmail.com>
2019-02-20 12:33:36 +0200

    ConnecttingService testiluokka tehty androidosioon

---
+++ android/src/com/mygdx/game/ConnectingService.java
@@ -0,0 +1,45 @@
+package com.mygdx.game;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+
+import com.wahoofitness.connector.HardwareConnector;
+import com.wahoofitness.connector.HardwareConnectorEnums;
+import com.wahoofitness.connector.HardwareConnectorTypes;
+import com.wahoofitness.connector.conn.connections.SensorConnection;
+
+public class ConnectingService extends Service {
+
+    private HardwareConnector mHardwareConnector ;
+    private final HardwareConnector . Listener mHardwareConnectorListener = new HardwareConnector . Listener (){
+        @Override
+        public void onHardwareConnectorStateChanged(HardwareConnectorTypes.NetworkType networkType, HardwareConnectorEnums.HardwareConnectorState hardwareConnectorState) {
+
+        }
+
+        @Override
+        public void onFirmwareUpdateRequired(SensorConnection sensorConnection, String s, String s1) {
+
+        }
+    };
+
+    @Override
+    public void onCreate () {
+        super . onCreate ();
+        mHardwareConnector = new HardwareConnector ( this , mHardwareConnectorListener );
+    }
+
+    @Override
+    public void onDestroy () {
+        super . onDestroy ();
+        mHardwareConnector . shutdown ();
+    }
+
+    @androidx.annotation.Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+}
