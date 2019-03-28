cf2a1ef74a6d259e818f8bf503b177850e26e19e
Miika <miika.m.minkkinen@gmail.com>
2019-03-28 14:32:29 +0200

    Merge branch 'master' of C:\Users\batte\Desktop\Jurassic Doom Cycling Extreme with conflicts.

---
+++ core/src/com/mygdx/game/LevelSelectScreen.java
@@ -0,0 +1,12 @@
+package com.mygdx.game;
+
+public class LevelSelect extends NewScreen {
+    public LevelSelect(JDCEGame g) {
+        super(g);
+    }
+
+    @Override
+    public void render(float delta) {
+        
+    }
+}
