d98d36fb91c2d4018b8e96d51ffbea1e69848bca
Miika <miika.m.minkkinen@gmail.com>
2019-03-29 14:07:47 +0200

    FinishView constructor jne

--- core/src/com/mygdx/game/FinishView.java
+++ core/src/com/mygdx/game/FinishView.java
@@ -2,19 +2,33 @@ package com.mygdx.game;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Preferences;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.freetype.FreeType;
+import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
+import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator.FreeTypeFontParameter;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 
 public class FinishView extends NewScreen {
     Preferences highscores;
     private Texture background;
+    float finishTime;
+    private FreeTypeFontGenerator generator;
+    private FreeTypeFont font12;
 
-    public FinishView(JDCEGame g) {
+    public FinishView(JDCEGame g, float time) {
         super(g);
-        highscores = Gdx.app.getPreferences("highscores");
+        highscores = Gdx.app.getPreferences("JDCE_highscores");
         background = new Texture(Gdx.files.internal("bluebackground.png"));
+        addHighScore(Utilities.secondsToString(time));
 
+        generator = new FreeTypeFontGenerator(Gdx.files.internal("font.ttf"));
+        FreeTypeFontParameter parameter = new FreeTypeFontParameter();
+        parameter.size = 12;
+        parameter.borderColor = Color.BLACK;
+        parameter.borderWidth = 3;
+        font12 = generator.generateFont(parameter);
     }
 
     @Override
@@ -28,8 +42,8 @@ public class FinishView extends NewScreen {
         //getGameStage().draw();
     }
 
-    public void addHighScore(float f) {
-        highscores.putFloat("highscore", f);
+    public void addHighScore(String score) {
+        highscores.putString("High Score", score);
         highscores.flush();
     }
 }
