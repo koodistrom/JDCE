db20dc5174fb47f7113b26cf06f370e99336f6ea
Miika <miika.m.minkkinen@gmail.com>
2019-04-21 11:31:56 +0300

    LevelSelectScreen: levelpainikkeet muuttuu maan mukaan 1-40 ja ovat nyt tablessa

--- core/src/fi/tamk/jdce/LevelSelectScreen.java
+++ core/src/fi/tamk/jdce/LevelSelectScreen.java
@@ -2,6 +2,7 @@ package fi.tamk.jdce;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 
@@ -10,35 +11,64 @@ import java.util.ArrayList;
 public class LevelSelectScreen extends NewScreen {
     private float heightUnit = getStageHeight() / 3;
     private float widthUnit = getStageWidth() / 4;
-    private float levelSelectTextButtonWidth = getTextButtonHeight();
-    private float levelSelectTextButtonHeight = levelSelectTextButtonWidth;
+    private float levelSelectTextButtonWidth;
+    private float levelSelectTextButtonHeight;
     private float textButtonX = getStageWidth() / 2 - (getTextButtonWidth() / 2);
     private float textButtonY1 = getStageHeight() / 2 - (getTextButtonHeight() / 2);
     private float textButtonY2 = getStageHeight() / 2.5f - (getTextButtonHeight() / 2);
     private ArrayList<ArrayList<TextButton>> buttonGrid;
     private int levelnumber;
     private int worldNumber;
+    private Table levelTable;
 
     public LevelSelectScreen(JDCEGame g, final int worldNumber) {
         super(g);
+
+        setupButtonBounds();
+
+        levelTable = new Table();
+        levelTable.defaults().pad(17.5f).space(17.5f);//.size(levelSelectTextButtonWidth, levelSelectTextButtonHeight);
+
         this.worldNumber = worldNumber;
         int posX;
         int posY;
-        levelnumber = 1;
+
+        switch(worldNumber) {
+            case 1:
+                levelnumber = 1;
+                break;
+            case 2:
+                levelnumber = 11;
+                break;
+            case 3:
+                levelnumber = 21;
+                break;
+            case 4:
+                levelnumber = 31;
+                break;
+        }
+
+        //levelnumber = 1;
         buttonGrid = new ArrayList<ArrayList<TextButton>>();
-        for(int i = 0; i<2;i++){
+        for(int i = 0; i<5;i++){
+            levelTable.row();
             buttonGrid.add(new ArrayList<TextButton>());
-            for(int n=0; n<3;n++){
+            for(int n=0; n<2;n++){
                 posX = n + 1;
                 posY = i + 1;
+
+
                 buttonGrid.get(i).add( new TextButton(getGame().getBundle().get("level") + " " + levelnumber, getGame().getUiSkin()));
 
-                buttonGrid.get(i).get(n).setWidth(levelSelectTextButtonWidth);
-                buttonGrid.get(i).get(n).setHeight(levelSelectTextButtonHeight);
-                buttonGrid.get(i).get(n).setPosition(widthUnit*posX, (heightUnit*posY)/*+1*/);
 
-                getGameStage().addActor(buttonGrid.get(i).get(n));
+                //buttonGrid.get(i).get(n).setHeight(levelSelectTextButtonHeight);
 
+
+                levelTable.add(buttonGrid.get(i).get(n)).size(levelSelectTextButtonWidth, levelSelectTextButtonHeight);
+
+                //buttonGrid.get(i).get(n).setPosition(widthUnit*posX, (heightUnit*posY)/*+1*/);
+
+                //buttonGrid.get(i).get(n).setSize(levelSelectTextButtonWidth, levelSelectTextButtonHeight);
 
                 buttonGrid.get(i).get(n).addListener(new ClickListener() {
                     int level = levelnumber;
@@ -58,15 +88,16 @@ public class LevelSelectScreen extends NewScreen {
             }
         }
 
-
-        setupButtonBounds();
         setupButtons();
 
+        getGameStage().addActor(levelTable);
         getGameStage().addActor(getMuteMusicButton());
         getGameStage().addActor(getMuteSoundFxButton());
         getGameStage().addActor(getBackButton());
-        getGameStage().setDebugAll(true);
+        //getGameStage().setDebugAll(true);
         Gdx.input.setInputProcessor(getGameStage());
+
+        updateTables();
 
         clickListeners();
 
@@ -111,6 +142,7 @@ public class LevelSelectScreen extends NewScreen {
         setupButtons();
     }*/
 
+    @Override
     public void setupButtonBounds() {
         super.setupButtonBounds();
         /*updateTenths();
@@ -127,30 +159,40 @@ public class LevelSelectScreen extends NewScreen {
         setMuteMusicX(getStageWidthTenth() * 9 - (getImageButtonWidth() / 2));
         setMuteSoundEffectsX(getMuteMusicX());*/
 
-        levelSelectTextButtonHeight = getTextButtonHeight();
-        levelSelectTextButtonWidth = levelSelectTextButtonHeight;
+        levelSelectTextButtonHeight = getStageHeightTenth();
+        levelSelectTextButtonWidth = getStageWidthTenth() * 1.75f;
 
-        heightUnit = getStageHeight() / 3;
-        widthUnit = getStageWidth() / 4;
+        heightUnit = getStageHeightTenth() * 2.5f;
+        widthUnit = getStageWidthTenth() * 2;
         textButtonX = getStageWidth() / 2 - (getTextButtonWidth() / 2);
         textButtonY1 = getStageHeight() / 2 - (getTextButtonHeight() / 2);
         textButtonY2 = getStageHeight() / 2.5f - (getTextButtonHeight() / 2);
     }
 
+    @Override
+    public void updateTables() {
+        levelTable.defaults().pad(17.5f).space(17.5f);
+        levelTable.setSize(getStageWidth() / 3, getStageHeightTenth() * 8);
+
+        levelTable.setPosition(getGameStage().getWidth() / 2 - (levelTable.getWidth() / 2),
+                getGameStage().getHeight() / 2 - (levelTable.getHeight() / 2));
+    }
+
+    @Override
     public void setupButtons() {
         super.setupButtons();
         levelnumber = 1;
         int posX;
         int posY;
 
-        for(int i = 0; i<2;i++){
-            for(int n=0; n<3;n++){
+        for(int i = 0; i<5;i++){
+            for(int n=0; n<2;n++){
                 posX = n + 1;
                 posY = i + 1;
 
-                buttonGrid.get(i).get(n).setWidth(levelSelectTextButtonWidth);
-                buttonGrid.get(i).get(n).setHeight(levelSelectTextButtonHeight);
-                buttonGrid.get(i).get(n).setPosition(widthUnit*posX, (heightUnit*posY)/*+1*/);
+                //buttonGrid.get(i).get(n).setSize(levelSelectTextButtonWidth, levelSelectTextButtonHeight);
+                //buttonGrid.get(i).get(n).setHeight(levelSelectTextButtonHeight);
+                //buttonGrid.get(i).get(n).setPosition(widthUnit*posX, (heightUnit*posY)/*+1*/);
 
                 levelnumber++;
 
